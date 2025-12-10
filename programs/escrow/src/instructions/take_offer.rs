6bf7a58b958e9c4bd5c958be45c31034feae9b6b
Daniel Koh <dan19980109@gmail.com>
2025-12-11 00:47:01 +0100

    test completed. no conflict

--- programs/escrow/src/instructions/take_offer.rs
+++ programs/escrow/src/instructions/take_offer.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-use anchor_spl::{associated_token::AssociatedToken, token, token_interface::{close_account, transfer_checked, CloseAccount, Mint, TokenAccount, TokenInterface, TransferChecked}};
+use anchor_spl::{associated_token::AssociatedToken, token_interface::{close_account, transfer_checked, CloseAccount, Mint, TokenAccount, TokenInterface, TransferChecked}};
 
 use crate::state::Offer;
 
